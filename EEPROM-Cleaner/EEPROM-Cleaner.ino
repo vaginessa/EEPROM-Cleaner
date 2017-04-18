5c356507df8263106dd9ba023e395fa1ad9014fe
Helmut <helmut.stult@schinfo.de>
2017-04-19 01:38:06 +0200

    v1.1 - optimized for esp8266_deauther
    It's only for cleaning the EEPROM-setting (SSID, Password, ...) of "esp8266_deauther" from spacehuhn.
    After using this sketch, you have a clean ESP8266.

--- EEPROM-Cleaner/EEPROM-Cleaner.ino
+++ EEPROM-Cleaner/EEPROM-Cleaner.ino
@@ -1,6 +1,6 @@
 /********************************************************************************************
 
-                       EEPROM-Cleaner v1.0
+                       EEPROM-Cleaner v1.1
 
                        Copyright (c) 2017 Helmut Stult (schinfo)
 
@@ -9,32 +9,64 @@
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
 
-#define maxByte 1000 // change it for more or less Bytes (Default = 1000)
+#define minByte 4096 // change it for more or less Bytes (Default = 4096)
+#define maxByte 1200 // change it for more or less Bytes (Default = 1200)
+#define startByte 33 // change it for more or less Bytes (Default = 33)
 
 unsigned long ok = 0;
+unsigned long nok = 0;
+unsigned long tok = 0;
+
 
 void setup()
 {
   Serial.begin(115200);
-  EEPROM.begin(512);
+  EEPROM.begin(minByte);
 
   delay(100);
 
   Serial.println("**************************************************************************");
   Serial.println("");
-  Serial.print("             Write a ""0"" to first ");
+  Serial.print("             Write a ""0"" from ");
+  Serial.print(minByte);
+  Serial.print(" to first ");
   Serial.print(maxByte);
   Serial.print(" bytes of the EEPROM");
   Serial.println("");
   Serial.println("");
-  Serial.println("             Start clearing EEPROM... - Please wait!!!");
+  Serial.println("**************************************************************************");
+  Serial.println("");
+
+  Serial.println("             testing EEPROM for written bytes");
+  Serial.println("");
+
+  for (int i = startByte; i < maxByte; ++i)
+  {
+    if (EEPROM.read(i) == 255) {
+      ++ok;
+    } else {
+      ++nok;
+    }
+  }
+  
+  Serial.print("                 empty bytes:   ");
+  Serial.println(ok);
+  Serial.print("             not empty bytes:   ");
+  Serial.println(nok);
   Serial.println("");
   Serial.println("**************************************************************************");
   Serial.println("");
 
+  Serial.println("**************************************************************************");
+  Serial.println("");
+  Serial.println("             Start clearing EEPROM... - Please wait!!!");
+  Serial.println("");
+  Serial.println("**************************************************************************");
+
+  delay(3000);
 
   // write a 0 to first maxByte bytes of the EEPROM
-  for (int i = 0; i < maxByte; ++i) {
+  for (int i = startByte; i < maxByte; ++i) {
     EEPROM.write(i, -1);
   }
 
@@ -42,20 +74,21 @@ void setup()
 
   delay(2000);
 
+  Serial.println("");
   Serial.println("             testing EEPROM for clearing");
   Serial.println("");
 
   String test;
-  for (int i = 0; i < maxByte; ++i)
+  for (int i = startByte; i < maxByte; ++i)
   {
     if (EEPROM.read(i) == 255) {
-      ++ok;
+      ++tok;
     }
     test += char(EEPROM.read(i));
   }
   Serial.println("**************************************************************************");
   Serial.println("");
-  if (ok = maxByte - 1) {
+  if (tok = maxByte - startByte) {
     Serial.println("             EEPROM killed correctly");
   } else
     Serial.println("             EEPROM not killed - ERROR !!!");
--- README.md
+++ README.md
@@ -4,19 +4,22 @@ EEPROM-Cleaner for ESP8266-Boards
 ![EEPROM-Cleaner for ESP8266-Boards](https://raw.githubusercontent.com/schinfo/EEPROM-Cleaner/master/screenshots/EEPROM-Cleaner.jpg)
 
 ## Contents
-- [How it works](#how-it-works)
 - [Disclaimer](#disclaimer)
+- [How it works](#how-it-works)
 - [License](#license)
-
-### How it works
-
-It erase the first, with maxByte defined xxx Bytes, in the EEPROM of an ESP8266 - LoLin, etc.   
 
 
 ## Disclaimer
 
+It's only for cleaning the EEPROM-setting (SSID, Password, ...) of "esp8266_deauther" from spacehuhn.
+After using this sketch, you have a clean ESP8266.
+
 You use it for your own risk!  
 
+
+## How it works
+
+It erase the first, beginning with minByte + startByte, with maxByte defined xxx Bytes, in the EEPROM of an ESP8266 - LoLin, etc.   
 
 
 ## License
@@ -24,4 +27,3 @@ You use it for your own risk!
 This project is licensed under the MIT License - see the [license file](LICENSE) file for details.
 
 **The License file must be included in any redistributed version of this program**
-
